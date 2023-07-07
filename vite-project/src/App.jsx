277046e8d0440163c37634e63aae424248b4f28a
Thiago <thiago_murakami@hotmail.com>
2023-07-07 20:51:53 -0300

    Create sidebar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,31 @@
-import {Post} from './Post'
 import {Header} from './components/Header'
+import {Post} from './Post'
+
+import styles from './App.module.css'
 
 import './global.css'
+import {Sidebar} from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Thiago Murakami"
-        content="I am the new Data Analytics"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="thiago murakami"
+            content="What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <Post 
+            author="thiago murakami"
+            content="What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+        </main>
+      </div>
     </div>
   )
 }
 
-export default App
