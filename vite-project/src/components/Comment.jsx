1389306584192f82b38915d16b4113d667f1cb74
Thiago <thiago_murakami@hotmail.com>
2023-07-19 11:27:24 -0300

    estilizando o comentario

---
+++ vite-project/src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/th1ago.png" alt="avatar" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerda de 1h</time>
+                        </div>
+                        <button title='Deletar comentario'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Thiago, omedeto!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(){
@@ -23,6 +24,20 @@ export function Post(){
                     <a href="">#nlw</a>{' '}
                     <a href="">#rocketseat</a>
                 </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe um comentario' />
+                <footer>
+                    <button type="submit">Publish</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
