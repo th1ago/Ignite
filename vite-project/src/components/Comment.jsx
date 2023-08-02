ffe2864368d0c1270b7e4c2a4c949785bbc11b69
Thiago <thiago_murakami@hotmail.com>
2023-08-02 18:23:50 -0300

    add comment

--- vite-project/src/components/Comment.jsx
+++ vite-project/src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment(){
+export function Comment({comment}){
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/th1ago.png"/>
@@ -16,7 +16,7 @@ export function Comment(){
                             <Trash size={24} />
                         </button>
                     </header>
-                    <p>Muito bom Thiago, omedeto!!</p>
+                    <p>{comment}</p>
                 </div>
 
                 <footer>
