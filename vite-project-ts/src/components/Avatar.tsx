a6c222c6cdc008990e881b60454b91ee5d38bea3
Thiago <thiago_murakami@hotmail.com>
2023-08-23 10:08:19 -0300

    .

--- vite-project-ts/src/components/Avatar.tsx
+++ vite-project-ts/src/components/Avatar.tsx
@@ -1,7 +1,8 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
 // ao inserir ? torna a propriedade opcional
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
     hasBorder: boolean;
     src: string;
     alt?: string
@@ -9,13 +10,13 @@ interface AvatarProps {
 
 // aplicando destruturacao
 // passando um obj como param informando buscando somente as propriedades que vou usar
-export function Avatar( {hasBorder = true, src}: AvatarProps) {
+export function Avatar( {hasBorder = true, src, alt}: AvatarProps) {
     return(
         <div>
             <img 
                 className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
                 src={src} 
-                alt="foto" 
+                alt={alt}
             />
         </div>
     )
