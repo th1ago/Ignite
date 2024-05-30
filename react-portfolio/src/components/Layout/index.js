7ccd41abc640273d1304b3eb4b1a624f2c7b438f
Thiago <thiago_murakami@hotmail.com>
2024-05-29 21:02:37 -0300

    feat: criando rotas

---
+++ react-portfolio/src/components/Layout/index.js
@@ -0,0 +1,22 @@
+
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <div className="App">
+      
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
